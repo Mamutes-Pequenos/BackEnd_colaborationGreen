7a78043a5cf5e36c2f4b4b67b897304a00099230
Emilio Anastácio <98831697+EmilioAnastacio@users.noreply.github.com>
2025-05-20 20:48:53 -0300

    Update SecurityConfiguration.java

--- src/main/java/com/greenLearning/greenlearning/config/security/SecurityConfiguration.java
+++ src/main/java/com/greenLearning/greenlearning/config/security/SecurityConfiguration.java
@@ -53,8 +53,7 @@ public class SecurityConfiguration {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("*");
-
+        config.addAllowedOrigin("http://34.31.216.100");
         config.setAllowedHeaders(Arrays.asList(HttpHeaders.AUTHORIZATION,HttpHeaders.CONTENT_TYPE,HttpHeaders.ACCEPT));
         config.setAllowedMethods(Arrays.asList(HttpMethod.GET.name(),HttpMethod.POST.name(),HttpMethod.PUT.name(),HttpMethod.DELETE.name()));
         config.setMaxAge(3600L);
